facad2f4f5641c8dbb87f306e8c0b96c089524d6
Taz <taz@Taz-Mbp.local>
2020-03-07 23:44:36 +0800

    almost stable

--- src/providers/prayertime/prayertime.ts
+++ src/providers/prayertime/prayertime.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from "rxjs";
+
 
 @Injectable()
 export class PrayertimeProvider {
@@ -18,7 +19,7 @@ export class PrayertimeProvider {
     this.zones = zone;
     //this.url = 'https://api.azanpro.com/times/today.json?zone='+ this.zones +'&format=12-hour';
     this.url = 'http://card.sendukbesi.net/?id='+ this.zones;
-  	return this.http.get(this.url)
+    return this.http.get(this.url)
     .map(res => res.json());
   }
 
